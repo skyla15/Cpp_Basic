3141bf3f4abe0ff48372ffdf87f4d15d2e236ae6
정재엽 <37529449+skyla15@users.noreply.github.com>
2018-09-04 22:05:21 +0900

    Update const.cpp

--- DataType/Const/const.cpp
+++ DataType/Const/const.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-
+#include "MY_CONSTANTS"
 /* 
  * const 사용 방법
  * 함수의 파라미터로 사용 ( 가독성을 위한 변경 방지 ) 
  * 런타임 상수시 사용 
- * 여러 상수 사용시 헤더파일에 몰아넣고 사용 
+ * 여러 상수 사용시 헤더파일에 몰아넣고 사용 ( namespace 이용 ) 
  * constexpr 사용하여 컴파일타임 상수 명확히 표현 
  
  */
@@ -27,13 +27,15 @@ void pritnNumber(const int my_number) {
 }
 
 int main() {
+	
+	int my_number = 10*constants::const1;
 
 	int number;
 	cin >> number;
-
 	const int special_number(number);
 	// 런타임 상수 
 	
 	system("pause");
+
 
 }
