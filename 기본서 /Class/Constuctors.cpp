b7048604cdbace65f81485fafe2982078e329a42
정재엽 <37529449+skyla15@users.noreply.github.com>
2018-09-06 22:48:52 +0900

    Update Constuctors.cpp

--- 기본서 /Class/Constuctors.cpp
+++ 기본서 /Class/Constuctors.cpp
@@ -1,6 +1,8 @@
 /*
   클래스 선언 방식
   1) 초기화 리스트를 사용하지 않는 경우 
+  - 기본생성자 / 디폴트 생성자 / private 생성자
+  - private 생성자 : 클래스 내부에서만 객체의 생성을 허용하려고 하는 경우 
   2) 초기화 리스트를 사용하는 경우 
 ******************* 소 괄 호 ***********************
      Class class {
@@ -41,13 +43,13 @@ class Time {
 public:
 	int hour, minute;
 	void print();
-
+	/*
 	Time() { // 일반 생성자 
 		hour = 0;
 		minute = 0;
 	}
 
-	/*
+	
 	Time(int h, int m) { // Overloading 생성자 
 		hour = h;
 		minute = m;
@@ -56,11 +58,15 @@ public:
 	time(int h = 0, int m = 0) { // default 생성자 
 		hour = h;
 		minute = m;
-	}
-	*/
+	}	
 
 	// 초기화 리스트 이용 
 	Time(int h, int m ) : hour{ h }, minute{ m } {}	
+	*/
+	
+	// private 생성자 
+	private:
+	Time(int h, int m) : hour{h}, minute{m} {}
 };
 
 void Time::print() {
