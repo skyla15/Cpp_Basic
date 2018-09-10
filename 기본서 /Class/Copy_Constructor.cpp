5390e2a8aa5d3e9ced71d487436b81a6f77810d7
Jay <37529449+skyla15@users.noreply.github.com>
2018-09-10 16:37:24 +0900

    Update Copy_Constructor.cpp

--- 기본서 /Class/Copy_Constructor.cpp
+++ 기본서 /Class/Copy_Constructor.cpp
@@ -1,74 +1,79 @@
-/*
-	RVO, 객체를 반환시킬 때, 객체를 반환하는 대신 인자를 직접 반환하여 반환되는 값을 받는
-	객체에 인자로 직접 넣어주는 방법 	
-*/
-
-#include "stdafx.h"
 #include <iostream>
-#include <cassert>
+#include <string>
 
-using namespace std; 
 
-class Fraction {
-private :
-	int m_numerator;
-	int m_denominator;
+using namespace std;
+
+class MyClass {
+private : 
+	std::string name;
 
 public :
-	Fraction(int num = 0, int den = 1) : m_numerator(num), m_denominator(den) {
-		assert(den != 0);
-		// den != 0 인경우 true, 0일 경우 프로그램 종료 
+	MyClass(string n = "NoName"): name{ n } {
+		cout << name << "생성되었습니다" << endl;
+		cout << endl;
+	}
+	
+	// Default Copy Constructor 
+	MyClass(const MyClass& mc) {
+		name = mc.name; 
+		cout << name << "복사되었습니다" << endl;
+		cout << endl;
 	}
 
-	Fraction(const Fraction &fraction) // copy constructor 
-		: m_numerator(fraction.m_numerator), m_denominator(fraction.m_denominator) {
-		cout << "Copy Constructor Called" << endl;
+	~MyClass() {
+		cout << name << "의 소멸자 호출" << endl;
+		cout << endl;
 	}
 
-	friend std::ostream & operator << (std::ostream & out, const Fraction & f) {
-		out << f.m_numerator << " / " << f.m_denominator << endl;
-		return out;
-	}
-
-	~Fraction() {
-		cout << "소멸자 호출" << endl;
-	}
 };
 
-Fraction doSomething() {
-	Fraction temp(1, 2);
-	cout << "&temp : " << &temp << endl;
+void update(MyClass mc) {
+	cout << "update 호출" << endl;
+	cout << endl;
+}
+
+// NRVO : MyClass객체 temp를 생성후 반환 
+// Release모드에서만 최대최적화
+MyClass doSomething() {
+	MyClass temp("테엠프");
+	//3 테엠프 생성 
 	return temp;
+	//4 테엠프 복사 <== myClass3(temp)
+	//5 테엠프 소멸
+	/*
+		Release 모드 컴파일시 4,5번 생략되면서 컴파일됨 ( NRVO ) 
+		릴리즈 모드로 컴파일 할경우 temp를 객체로 넘기는 것이 아니라  인자를 바로 넘겨줌
+		따라서 myClass3(temp)가 아닌 myClass3("테엠프") 로 값이 들어가게됨
+	*/
+}
+
+// RVO : 이름없는 반환값 최적화 
+// Debug모드에서도 작동을 함
+MyClass doSomething2() {
+	return MyClass("이름없는 테엠프");
+	//7 이름없는 테엠프 생성 
 }
 
 int main() {
+	MyClass myClass1("정재엽");
+	//1 정재엽 생성
+	MyClass myClass2(myClass1);
+	//2 정재엽 복사 
+	MyClass myClass3 = doSomething();
 
-	Fraction result = doSomething();
-	cout << "&result : " << &result << endl;
-	cout << "result : " << result << endl;
+	MyClass myClass4(MyClass("왓업"));
+	//6 왓업 생성	
 
-	// result의 복제 생성자가 실행이 되며 temp에서 넘어온 값들이 temp에 저장이 됨 
-	// 디버그 모드에서는 temp 와 result의 주소값이 다르기 때문에 복제생성자 호출 
-	// 이후 doSomething에서 
-	/*
-		릴리즈 모드에서는 두 값이 같도록 나옴 => Return Value Optimization ( RVO ) 
-		=> result 자체를 temp가 되도록 함 
-		=> 컴파일러가 최적화 하기위해 복제생성자 실행을 생략할 수도 있음 
-	*/
+	MyClass myClass5 = doSomething2();
+	// myClass4와 같은 식으로 컴파일됨
+	// myClass5(MyClass("이름없는 테엠프"))
+	// 컴파일러가 복사생성자가 아닌 바로 생성자 호출 
+	
 
-	cout << endl;
-
-	Fraction fr_copy(Fraction(3, 10)); 
-	cout << fr_copy << endl;
-	/*
-		컴파일러에 의하여 복사생성자가 필요없을 경우 생략 시킴 
-		복사생성자를 컴파일러가 자동으로 생략시킴
-
-	*/
-
-	Fraction fr_copy2(fr_copy);
-	cout << fr_copy2 << endl;
-	// 복제생성자 호출되어 fr_copy의 멤버변수들이 fr_copy2의 멤버변수로 복사가 되어 들어옴 
-
+	//8 이름없는 테엠프 소멸 
+	//9 왓업 소멸 
+	//10 => myClass3으로 복사된 테엠프 소멸 
+	//11 => myClass2로 복사된 정재엽 소멸 
+	//12 myClass1 정재엽 소멸 소멸 
 }
-
