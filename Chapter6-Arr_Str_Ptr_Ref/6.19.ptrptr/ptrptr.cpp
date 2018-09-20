96fa7aa06e56a4cd287219f561f8dd30a99dbf7b
Jay <37529449+skyla15@users.noreply.github.com>
2018-09-20 23:27:29 +0900

    Update ptrptr.cpp

--- Chapter6-Arr_Str_Ptr_Ref/6.19.ptrptr/ptrptr.cpp
+++ Chapter6-Arr_Str_Ptr_Ref/6.19.ptrptr/ptrptr.cpp
@@ -1,6 +1,6 @@
 // 이중 포인터 
 /*
-	이중배열 구현시 사용 
+	1) 이중배열 구현시 사용 
 */
 
 #include <iostream>
@@ -18,6 +18,26 @@ int main()
 	};
 
 	/*****************************************************/
+	/*         1차원 배열을 2차원 배열처럼 사용          */
+	/*         [r][c] = c + r*col                        */
+
+	int *matrix_2 = new int[row * col];
+	for (int r = 0; r < row; r++)
+		for (int c = 0; c < col; c++)
+			matrix_2[c + col * r] = s2da[r][c];
+
+	for (int r = 0; r < row; r++) {
+		for (int c = 0; c < col; c++)
+		{
+			cout << matrix_2[col*r + c] << " ";
+		}
+		cout << endl;
+	}
+
+
+	/*****************************************************/
+	/*           2차원 포인터 사용방법 1                 */
+
 
 	int **matrix = new int*[row];	
 		// 메모리 해제시 : r1~3 -> matrix 
@@ -50,6 +70,7 @@ int main()
 	delete[] matrix;
 
 	/*****************************************************/
+	/*           2차원 포인터 사용방법 2                 */
 
 
 	int *r1 = new int[col] {1, 2, 3, 4, 5};
