e700df47d58eac157d0412e25a3dac9149dd4606
Jay <37529449+skyla15@users.noreply.github.com>
2018-10-15 18:27:24 +0900

    Create prob_4.cpp

---
+++ Assignment/Week2/prob_4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+	// int x;
+	int x = 2; // x를 2로 초기화 합니다 
+	int y = 10;
+
+	cout << "두 수의 합은 : " << x + y << endl;
+	system("pause");
+	return 0;
+}
