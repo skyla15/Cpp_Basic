d098f16cce37fd0feea5b18aa768f6b3a82b5bbe
Jay <37529449+skyla15@users.noreply.github.com>
2018-10-15 18:26:41 +0900

    Create prob_3.cpp

---
+++ Assignment/Week2/prob_3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+	int x{ 10 };
+	cout << "int형 변수 x의 값 : " << x << endl; // x에 저장된 값을 출력
+	cout << "int형 변수 x의 주소값 : " << &x << endl; // 변수 x가 위치한 주소 
+
+	return 0;
+}
