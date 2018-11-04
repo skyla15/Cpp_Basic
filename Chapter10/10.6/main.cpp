7180d498076f9285420e56a6a39b037a7b45b802
Jay <37529449+skyla15@users.noreply.github.com>
2018-11-04 23:35:20 +0900

    Create main.cpp

---
+++ Chapter10/10.6/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+class IntArray
+{
+private:
+	int m_length = 0;
+	int *m_dat = nullptr
+public:
+	// constructor
+	// Destructor
+	// initializer
+	// reset
+	// resize
+	// insertBefor(const int & value, const int & ix)
+	// remove(const int & ix)
+	// push_back(const int & value)
+
+};
+
+
+int main()
+{
+	// IntArra my_arr{1,3,5,7,9}
+	// my_arr.insertBefor(10, 1) // 1, 10, 3, 5, 7, 9
+	// my_arr.remove(3) // 1, 10, 5, 7, 9
+	// my_arr.push_back(13) // 1,10,3,7,9,13
+
+}
